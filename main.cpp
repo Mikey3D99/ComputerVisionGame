c920276723e68db0c48c3230aa072a7242baa9d5
Jnwyk <nowak.jan@yahoo.com>
2021-11-04 21:09:59 +0100

    added contours to the ball

--- main.cpp
+++ main.cpp
@@ -4,6 +4,42 @@
 #include <iostream>
 
 using namespace cv;
+using namespace std;
+
+void getContours(Mat imgDil, Mat img) {
+
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+
+    findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+//    drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+
+    vector<vector<Point>> conPoly(contours.size());
+    vector<Rect> boundRect(contours.size());
+
+    for (int i = 0; i < contours.size(); i++){
+        int area = contourArea(contours[i]);
+        cout << "Area: " << area << endl;
+        string objectType;
+
+        if (area > 1000){
+            float peri = arcLength(contours[i], true);
+            approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+            cout << conPoly[i].size() << endl;
+            boundRect[i] = boundingRect(conPoly[i]);
+
+            int objCor = (int)conPoly[i].size();
+
+            if (objCor > 4){
+                objectType = "Circle";
+                cout << "Mamy kolko" << endl;
+            }
+
+//            drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+            rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(255, 0, 255), 20);
+        }
+    }
+}
 
 int main(){
 
@@ -22,16 +58,35 @@ int main(){
 //    createTrackbar("Val Min", "Trackbars", &vmin, 255);
 //    createTrackbar("Val Max", "Trackbars", &vmax, 255);
 
+    Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
+
     while(1){
         cap.read(img);
         cvtColor(img, imgHSV, COLOR_BGR2HSV);
+
+        //ustawienie koloru
         Scalar lower(hmin, smin, vmin);
         Scalar upper(hmax, smax, vmax);
-
         inRange(imgHSV, lower, upper, mask);
+        imshow("Mask", mask);
+
+
+        GaussianBlur(mask, imgBlur, Size(5, 5), 15, 15);
+
+        imshow("Image", imgBlur);
+        Canny(imgBlur, imgCanny, 25, 75);
+        imshow("Image Canny", imgCanny);
+
+        Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+
+        dilate(imgCanny, imgDil, kernel);
+        imshow("Image dilate", imgDil);
+
+        getContours(imgDil,img);
+
         imshow("Image", img);
 //        imshow("Image HSV", imgHSV);
-        imshow("Mask", mask);
+
         waitKey(1);
     }
 
